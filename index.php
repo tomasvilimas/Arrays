9c7a435e21c49a72b05e8a2026c542d180e665ea
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2021-02-24 17:03:43 +0200

    Arrays

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+print("<pre>");
+
+$arejus = array(
+
+"Tomas" => "75",
+"Antanas" => "85",
+"Jonas" => "35",
+"Petras" => "120",
+"Tadas" => "92"
+);
+
+print($arejus['Tomas'] .  "  aš");
+
+print("<br>");
+
+print(min($arejus). "  lengviausias");
+
+print("<br>");
+
+print(max($arejus). "  sunkiausias");
+
+print("<br>");
+
+if(array_sum($arejus) <500)
+print(array_sum($arejus). "  tinkamas svoris tilpti i lifta");
+else if (print("Svoris per didelis"));
+
+print("<br>");
+
+print(sort($arejus));
+
+
+
+print("</pre>");
+
+?>
+
+</body>
+</html>
